ee04f9541ec0df62b0a53c388307854282851274
Adrián Flores <adriandev.fl@gmail.com>
2026-04-22 18:18:14 -0300

    fix: scroll to top behavio

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { ScrollToTop } from '@/components/scroll-to-top';
 import { HousesPage } from '@/pages/houses-page';
 import { SavedPage } from '@/pages/saved-page';
 import { ComparisonPage } from '@/pages/comparison-page';
@@ -7,12 +8,15 @@ import { NotFoundPage } from '@/pages/not-found-page';
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HousesPage />} />
-      <Route path="/saved" element={<SavedPage />} />
-      <Route path="/studio" element={<ComparisonPage />} />
-      <Route path="/houses/:id" element={<HouseDetailPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HousesPage />} />
+        <Route path="/saved" element={<SavedPage />} />
+        <Route path="/studio" element={<ComparisonPage />} />
+        <Route path="/houses/:id" element={<HouseDetailPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
